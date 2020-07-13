82975b338a0135bfb26878f434c3cd2a614e20bb
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-13 23:41:16 +0600

    changed url to https

--- src/client/web/buy-manas/src/api/api.js
+++ src/client/web/buy-manas/src/api/api.js
@@ -1,7 +1,7 @@
 import * as axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://buymanasapi.ru.xsph.ru/index.php/api/', 
+    baseURL: 'https://buymanasapi.ru.xsph.ru/index.php/api/', 
 })
 
 
