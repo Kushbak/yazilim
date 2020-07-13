3f2d0b696a83cb7c62679c0c65df05d7530bce86
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-13 20:47:08 +0600

    Updates

---
+++ precache-manifest.8ab9ed06671cea98953061b5de2c8a22.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2521a78287b051252779df97f1d7b5",
+    "url": "/BuyManas/index.html"
+  },
+  {
+    "revision": "808620151b638bde721e",
+    "url": "/BuyManas/static/css/2.511ceea3.chunk.css"
+  },
+  {
+    "revision": "ac231cc8d7e3e1229230",
+    "url": "/BuyManas/static/css/main.4cbbc7f2.chunk.css"
+  },
+  {
+    "revision": "808620151b638bde721e",
+    "url": "/BuyManas/static/js/2.10fbc09d.chunk.js"
+  },
+  {
+    "revision": "b9d39063624d4ecd55ea544014cdccac",
+    "url": "/BuyManas/static/js/2.10fbc09d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac231cc8d7e3e1229230",
+    "url": "/BuyManas/static/js/main.e01e968f.chunk.js"
+  },
+  {
+    "revision": "a0eb471d3021222377bf",
+    "url": "/BuyManas/static/js/runtime-main.7631ccc3.js"
+  },
+  {
+    "revision": "06a19a797d63ec52d8ec8222d6c42c93",
+    "url": "/BuyManas/static/media/1.06a19a79.png"
+  },
+  {
+    "revision": "2c83efbc82fd77fe19ef589afb35e5c3",
+    "url": "/BuyManas/static/media/10.2c83efbc.png"
+  },
+  {
+    "revision": "4f13c2d8315402b53adf224915fcf3bd",
+    "url": "/BuyManas/static/media/11.4f13c2d8.png"
+  },
+  {
+    "revision": "327a4d9af8f718c603994096f88040f4",
+    "url": "/BuyManas/static/media/12.327a4d9a.png"
+  },
+  {
+    "revision": "7ec36eeb9f134d9c628aec014a1bbbad",
+    "url": "/BuyManas/static/media/13.7ec36eeb.png"
+  },
+  {
+    "revision": "dc9b7b2e9406d36717d68e0649397dad",
+    "url": "/BuyManas/static/media/14.dc9b7b2e.png"
+  },
+  {
+    "revision": "73782f16f207f00910b7a1c00ceee4d6",
+    "url": "/BuyManas/static/media/2.73782f16.png"
+  },
+  {
+    "revision": "4b2d3c9c4406da74fba8dd3e56d4ea9b",
+    "url": "/BuyManas/static/media/3.4b2d3c9c.png"
+  },
+  {
+    "revision": "5f13f5d74fa606b3a231378418aa3478",
+    "url": "/BuyManas/static/media/4.5f13f5d7.png"
+  },
+  {
+    "revision": "c3fc99473c2cc8a55d43f6aaa3816c28",
+    "url": "/BuyManas/static/media/5.c3fc9947.png"
+  },
+  {
+    "revision": "e0be08622ed593a5467fcc00ac6595d5",
+    "url": "/BuyManas/static/media/6.e0be0862.png"
+  },
+  {
+    "revision": "d6e383ac8b2d6e1e3c8639968e3a5f6c",
+    "url": "/BuyManas/static/media/7.d6e383ac.png"
+  },
+  {
+    "revision": "ecdf8018ec633b6e6c031686d7666a73",
+    "url": "/BuyManas/static/media/8.ecdf8018.png"
+  },
+  {
+    "revision": "125c032e9eaa3b42303a4aede7abfbea",
+    "url": "/BuyManas/static/media/9.125c032e.png"
+  },
+  {
+    "revision": "248f5763ee9608a333ab58453cdf1666",
+    "url": "/BuyManas/static/media/avatar.248f5763.jpg"
+  },
+  {
+    "revision": "5670893c4daf97eada4bffd0caa828f7",
+    "url": "/BuyManas/static/media/logo.5670893c.png"
+  },
+  {
+    "revision": "ce7a85bdec3fa45f052282958606ad3a",
+    "url": "/BuyManas/static/media/other1.ce7a85bd.png"
+  },
+  {
+    "revision": "5149883c75b4c3150dbd3c262740abdf",
+    "url": "/BuyManas/static/media/other2.5149883c.png"
+  },
+  {
+    "revision": "7ba9c915554880ed34efee3eaf65a756",
+    "url": "/BuyManas/static/media/preloader.7ba9c915.gif"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/BuyManas/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/BuyManas/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/BuyManas/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/BuyManas/static/media/slick.f97e3bbf.svg"
+  }
+]);
